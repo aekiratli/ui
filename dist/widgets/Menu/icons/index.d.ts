dfc4a77d9521336f38dace0bd9a9b71238f2d57d
aekiratli <emre719@gmail.com>
2021-04-16 14:16:23 +0300

    new kit

--- dist/widgets/Menu/icons/index.d.ts
+++ dist/widgets/Menu/icons/index.d.ts
@@ -20,5 +20,3 @@ export { default as AuditIcon } from "./Audit";
 export { default as GooseIcon } from "./Goose";
 export { default as LayerIcon } from "./Layer";
 export { default as RoadmapIcon } from "./Roadmap";
-export { default as GithubIcon } from "./Github";
-export { default as YoutubeIcon } from "./Youtube";
