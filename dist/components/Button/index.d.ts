a881cecff78771654308da2ca0e682174068ef87
aekiratli <emre719@gmail.com>
2021-05-02 23:58:50 +0300

    new kit

--- dist/components/Button/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as Button } from "./Button";
-export { default as IconButton } from "./IconButton";
-export type { ButtonProps, Scale as ButtonSizes, Variant as ButtonVariants } from "./types";
